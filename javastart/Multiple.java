a00f14c5081dc1923f08d4a232b2e31dad87ec12
Wonjun Seo <kk01057561299@gmail.com>
2025-04-01 17:43:41 +0900

    first commit

---
+++ javastart/Multiple.java
@@ -0,0 +1,12 @@
+package javastart;
+
+public class Multiple {
+
+	public static void main(String[] args) {
+		Multipl table = new Multipl();
+		table.printMultiplic();
+		// table.printMultiplic(7);
+	}
+
+}
+ 
